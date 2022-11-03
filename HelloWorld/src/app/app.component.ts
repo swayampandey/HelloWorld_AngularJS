c2d686832d418a95f687c619dcab56934c43c57e
Swayam Prakash <swayam265@gmail.com>
2022-11-04 00:55:48 +0530

    [Swayam] : added functionality to launch bridgelabz website in new tab on clicking bridgelabz logo

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -6,6 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "Hello from Bridgelabz";
+  title = "Hello World";
   imgUrl = "../assets/BridgeLabz.jpg"
+  url = "https://www.bridgelabz.com/";
+  ngOnInit(): void {
+    this.title = "Hello from Bridgelabz"
+  }
+
+  onClick($event: any) {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
 }
